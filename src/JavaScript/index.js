363a02237e000dc331c5ef2e52a44dedce7e3005
Hudson Faria <hudsonfariadev@gmail.com>
2023-06-16 15:51:42 -0300

    Add files via upload

---
+++ src/JavaScript/index.js
@@ -0,0 +1,16 @@
+
+const btn = document.querySelectorAll('.btn');
+console.log(btn);
+
+btn.forEach(function (button) {
+    button.addEventListener('click', function () {
+        this.classList.toggle('active');
+
+        if (button.classList.contains('active')) {
+            btn.forEach(function (button) {
+                button.classList.remove('active');
+            })
+            button.classList.toggle('active');
+        }
+    })
+})
